68637fe87559d3cbfb33ff6c31d603a63e4f7e48
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-10-03 08:33:08 -0300

    alguns resultados

---
+++ events.js
@@ -0,0 +1,28 @@
+// // let te = "0123456789";
+// let texto = "Mergulhando nas profundezas";
+// let texto2 = "Sofia Manzano";
+
+// console.log(texto.indexOf("o"));// pega o index do "o"
+// console.log(texto.toLocaleUpperCase().indexOf("O"));/// converte em maiusculo e pega o index do primeiro "O" maiusculo
+// console.log(texto.toLocaleUpperCase().substring(3,9).indexOf("g"));// converte em maiusculo depois corta e seleciona apenas a parte dentro substring no caso (3,9) e no fim pega o "g" minusculo que nao existe e o resultado desse console é -1;
+// console.log(texto.toLocaleUpperCase().substring(3,9).indexOf("g") + texto2.substring(2,3));// converte em maiusculo depois corta e seleciona apenas a parte dentro substring no caso (3,9) e no fim pega o "g" minusculo que nao existe e o resultado desse console é -1 adiciona mais um pedaco agora do texto 2 e o resultato sera "-1f";
+
+// console.log(texto.toLocaleUpperCase().substring(3,9).indexOf("g") + texto2.substring(2,3).indexOf("f"));// converte em maiusculo depois corta e seleciona apenas a parte dentro substring no caso (3,9) e no fim pega o "g" minusculo que nao existe e o resultado desse console é -1 adiciona mais um pedaco agora do texto 2 e agora se tornou uma soma por conta dos dois numeros gerados do indexOf;
+// console.log(texto.toLocaleUpperCase().substring(3,9).indexOf("g") + texto2.substring(2,3).indexOf("f")+"");// converte em maiusculo depois corta e seleciona apenas a parte dentro substring no caso (3,9) e no fim pega o "g" minusculo que nao existe e o resultado desse console é -1 adiciona mais um pedaco agora do texto 2 e agora se tornou um numero no final e se tornou um texto no fim.
+// console.log(texto.toLocaleUpperCase().substring(3,9).indexOf("g") +""+ texto2.substring(2,3).indexOf("f"));//resultado é texto por conta das contacatenações
+
+
+// let texto3 = "Sofia21 ABC";
+// console.log(texto3.split(",")); //retorna sempre um array dividindo dos elementos com base no que ta dentro do parentese do split
+// console.log(texto3.split(" ").join());//join sempre retorna uma string
+
+// let texto4 = "Sofia Manzano";
+
+// console.log(texto4.substring(0,5) + texto4.indexOf("f") + texto4.indexOf("o"));
+
+
+
+
+
+
+
